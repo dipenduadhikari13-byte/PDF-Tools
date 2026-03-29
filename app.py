05f4546994052b227b3bac9ea8a579b9b31fb1a2
Dipendu Adhikari <dipenduadhikari13@gmail.com>
2026-03-29 18:42:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(page_title="PDF Tools", page_icon="📄", layout="wide")
+
+st.title("📄 PDF Tools")
+
+st.markdown("""
+### Available tools:
+- PDF Compressor
+- PDF Splitter
+
+👉 Select a tool from sidebar
+""")
